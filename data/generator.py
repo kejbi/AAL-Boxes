03eb9993ae7b758d3fdca0b5459d8b486fec7a49
kejbi <biegajski.kacper@gmail.com>
2020-01-03 20:28:24 +0100

    generator_hard returns also median of times, time_test added

--- data/generator.py
+++ data/generator.py
@@ -16,7 +16,7 @@ def generate_boxes_hard(count, max_range):
     for i in range(0, count):
         x = random.randint(1, max_range)
         y = random.randint(1, max_range)
-        z = 0.987
+        z = 0.487 if random.random() > 0.92 else 0.643
         box = [x, y, z, volume(x, y, z)]
         boxes.append(rotate_box(box))
     return boxes
--- modes/modes.py
+++ modes/modes.py
@@ -1,6 +1,8 @@
 from data.reader import read_boxes_from_file
-from data.generator import generate_boxes
+from data.generator import generate_boxes, generate_boxes_hard
 from solution.solver import pack_boxes
+from time import time
+from statistics import mean, median
 
 def solve_from_input(filename):
     boxes = read_boxes_from_file(filename)
@@ -21,3 +23,19 @@ def generate_and_solve(filename, count, max_range):
             writer.write(f'{stack}\n-----------------------------------\n')
         
         writer.write(f'Total volume: {total_volume}')
+
+def time_test(start_count, step, k, r):
+    number_of_boxes = start_count
+    times = []
+    for i in range(0, k):
+        step_times = []
+        for j in range(0, r):
+            boxes = generate_boxes_hard(number_of_boxes, 10)
+            start = time()
+            pack_boxes(boxes)
+            end = time()
+            step_times.append(end-start)
+        times.append(mean(step_times))
+        number_of_boxes += step
+    return times, median(times)
+
